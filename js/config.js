629ff061a85454fb6a1504814e41598c9cb1fc9e
rizd027 <alfarizd027@gmail.com>
2026-01-23 10:59:35 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    // Google Apps Script Web App URL
+    SHEET_API_URL: 'https://script.google.com/macros/s/AKfycbzHTQzvQswWUEXgVAD6RMhkv_fLY8eKBbCnUi-h2lEG9hwnrNUjO4OaWnVcKUMkT2Lk/exec',
+    // Set to false to use Google Sheets, true to use localStorage for testing
+    USE_LOCAL_STORAGE: false
+};
